32ecc43e9c63841cb3e8a4248445f9f79ae2da93
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2023-08-23 19:32:15 -0400

    Imported code from published site.

---
+++ wwwroot/src/js/redirect.js
@@ -0,0 +1,8 @@
+﻿function reroute() {
+    var site = window.location;
+    if (site.search.length > 1) {
+        var newSite = site.origin;
+        newSite += site.search.substring(1);
+        site.replace(newSite);
+    }
+}
